8ef2eda802fe9523ff9a30f28adb78f897ee8107
francis <franrtg96@gmail.com>
2023-08-28 13:19:00 -0600

    created component and added it to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-
+import WeatherCard from '@/components/weatherCard'
 export default function Home() {
 
   return (
-    <main >
-      <div>
-        
+    <main className="">
+      <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <WeatherCard />
       </div>
     </main>
   )
