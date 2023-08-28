92623e6804ffcad287a5c5d3ce39c6fdf9700510
francis <franrtg96@gmail.com>
2023-08-28 13:46:10 -0600

    created first template of card design

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import WeatherCard from '@/components/weatherCard'
 export default function Home() {
 
   return (
-    <main className="">
-      <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <WeatherCard />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className=''>
+        <WeatherCard/>
       </div>
     </main>
   )
--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function WeatherCard() {
   return (
-    <div>weatherCard</div>
+    <div className=''>
+      <div className='border-2 border-black rounded p-8'>
+
+        <div className='py-2 flex justify-between  w-[20em] '>
+          <div>
+            place1
+          </div>
+          <div>
+            place2
+          </div>
+        </div>
+        <div className='py-2'>
+          place3
+        </div>
+        <div className='py-2'>
+          place4
+        </div>
+        <div className='py-2'>
+          place5
+        </div>
+
+      </div>
+    </div>
   )
 }
