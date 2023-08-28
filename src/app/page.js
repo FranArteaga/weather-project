bf2647fe67e2a7be64b4bfdcdd7e6bb2fc919c97
francis <franrtg96@gmail.com>
2023-08-28 17:09:10 -0600

    implemented data from the api to the card

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,30 @@
-import Image from 'next/image'
+'use client'
+import React, { useEffect, useState } from 'react'
 import WeatherCard from '@/components/weatherCard'
+
 export default function Home() {
+  let weatherUrl = process.env.NEXT_PUBLIC_WEATHER_URL
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    fetch(weatherUrl)
+      .then(response => response.json())
+      .then(data => {
+        setWeather(data)
+        console.log(data)
+      })
+  }, [])
+  console.log(weather)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className=''>
         <WeatherCard
-        city="tepic"
-        temperature="34"
-        weather="Cloudy"
-        humidity="hhhhh"
-        wind="wwwww"
+          city={weather.name}
+          temperature={weather.main ? `${weather.main.temp}°C` : null}
+          weather={weather.weather ? weather.weather[0].main : null}
+          humidity={weather.weather ? `${weather.main.humidity}%` : null}
+          wind={weather.weather ? `${weather.wind.speed}` : null}
 
         />
       </div>
