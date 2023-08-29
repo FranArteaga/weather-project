a17476631efefe1f51ab7432e4e2ba01564d262d
francis <franrtg96@gmail.com>
2023-08-28 21:24:59 -0600

    added more spacing between infor inside the card

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function WeatherCard(props) {
   const { city, temperature, weather, weatherDescription, humidity, wind, max, min } = props
   return (
-    <div className='text-black bg-white bg-opacity-90  min-h-[21em] border-2 border-black rounded-lg px-8 py-6 w-[20em]'>
+    <div className='text-black bg-white bg-opacity-90  min-h-[21em] border-2 border-black rounded-lg px-8 py-10 sm:py-6 w-[20em]'>
       <div className='flex flex-col justify-between font-bold'>
 
         <div className='flex flex-col items-center pb-2 '>
@@ -11,10 +11,10 @@ export default function WeatherCard(props) {
             {city}
           </div>
 
-          <div className='py-3 text-3xl  flex flex-col items-center'>
-            <div className='font-normal'>{temperature}</div>
-            <div className='font-medium'>{weather}</div>
-            <div className='font-medium'>{weatherDescription}</div>
+          <div className='py-6 sm:py-3 text-3xl  flex flex-col items-center'>
+            <div className='font-medium sm:font-normal pb-5 sm:pb-3'>{temperature}</div>
+            <div className='font-semibold sm:font-medium pb-2 sm:pb-0'>{weather}</div>
+            <div className='font-semibold sm:font-medium '>{weatherDescription}</div>
           </div>
 
 
